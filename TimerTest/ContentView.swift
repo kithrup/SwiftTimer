60553488a9c715244693171ac968379647628d48
Sean Eric Fagan <kithrup@mac.com>
2020-07-09 11:13:41 -0700

    Initial Commit

---
+++ TimerTest/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TimerTest
+//
+//  Created by Sean Eric Fagan on 7/9/20.
+//  Copyright © 2020 Kithrup Enterprises, Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
